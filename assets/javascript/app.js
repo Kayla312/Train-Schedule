bf7c6228d5f86ae761701292e00a086a7156bbda
The Singer <kayla312@harbornet.com>
2018-09-29 10:25:51 -0700

    added initialization to FireBase in my JavaScript file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBFKW8kmZDlcqgrb8HPoIxvSnj8PbiHQKs",
+    authDomain: "train-scheduler312.firebaseapp.com",
+    databaseURL: "https://train-scheduler312.firebaseio.com",
+    projectId: "train-scheduler312",
+    storageBucket: "train-scheduler312.appspot.com",
+    messagingSenderId: "95605707053"
+  };
+  firebase.initializeApp(config);
+  
 $("#add-train").on("click", function(event) {
     // prevent page from refreshing when form tries to submit itself
     event.preventDefault();
@@ -17,3 +28,4 @@ $("#add-train").on("click", function(event) {
     $("#destination-display").text(email);
     $("#time-display").text(age);
     $("#frequency-display").text(comment);
+});
